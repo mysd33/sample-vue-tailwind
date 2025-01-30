e8bdbded9d56698c818cc999fe93a3728752d7fc
mysd33 <mysd.33@gmail.com>
2025-01-30 22:12:40 +0900

    ログイン画面のStoryBookの例

---
+++ src/views/LoginView.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import LoginView from './LoginView.vue'
+
+/**
+ * ## ログイン画面のコンポーネント
+ */
+const meta: Meta<typeof LoginView> = {
+  component: LoginView,
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof LoginView>
+
+export const Default: Story = {
+  args: {},
+}
