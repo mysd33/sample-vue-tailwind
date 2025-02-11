1e5ffa6e18855c0a7fc33178ada14c799824c233
mysd33 <mysd.33@gmail.com>
2025-02-11 10:11:37 +0900

    ViewRouterのメタデータ定義の見直し

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,51 +6,47 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      // ログイン画面
+      // ホーム画面（ログイン画面）
       path: '/',
       name: 'home',
+      // ログイン未済でアクセスしてよい画面
+      meta: { allowsNotAuth: true },
       component: LoginView,
     },
     {
       // メニュー画面
       path: '/menu',
       name: 'menu',
-      meta: { requiresAuth: true },
       component: () => import('@/views/MenuView.vue'),
     },
     {
       // TODO管理画面
       path: '/todo',
       name: 'todo',
-      meta: { requiresAuth: true },
       component: () => import('@/views/TodoListView.vue'),
     },
     {
       // TODO一括登録
       path: '/todoFile/upload',
       name: 'todoFileUpload',
-      meta: { requiresAuth: true },
       component: () => import('@/views/TodoFileUploadView.vue'),
     },
     {
       // ユーザー管理画面
       path: '/users',
       name: 'userList',
-      meta: { requiresAuth: true },
       component: () => import('@/views/UserListView.vue'),
     },
     {
       // ユーザ登録画面
       path: '/newuser',
       name: 'newUser',
-      meta: { requiresAuth: true },
       component: () => import('@/views/UserRegisterView.vue'),
     },
     {
       // ユーザ詳細画面
       path: '/users/:id',
       name: 'userDetail',
-      meta: { requiresAuth: true },
       component: () => import('@/views/UserDetailView.vue'),
       props: (routes) => {
         return {
@@ -70,8 +66,8 @@ router.beforeEach((to, from, next) => {
     }
   }
 
-  // ログインが必要な直接画面に遷移しようとする場合に、ログイン済みかどうかをチェックする
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
+  // ログイン要の画面に直遷移しようとする場合に、ログイン済みかどうかをチェックする
+  if (!to.meta.allowsNotAuth) {
     const userStore = useUserStore()
     if (!userStore.isLoggedIn) {
       next({ name: 'home' })
