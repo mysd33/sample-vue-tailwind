3d65122445fc1d6a7e44581f53862d49defc1e6f
mysd33 <mysd.33@gmail.com>
2025-03-23 21:12:26 +0900

    ユーザ一覧のStoryBookの初期データ登録対応

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,6 +5,7 @@ import { App } from 'vue'
 import { configure } from 'vee-validate'
 import { setLocale } from 'yup'
 import * as ja from '@/usecases/common/validation/validation_messages_ja'
+import { useUserDummyStore } from '@/usecases/common/stores/usersStore'
 
 // Piniaの有効化
 const pinia = createPinia()
@@ -18,6 +19,10 @@ setup((app: App) => {
 
   // yupの入力エラーメッセージの日本語化
   setLocale(ja.messages)
+
+  // スタブ実装として、以下のユーザデータを初期登録する
+  const usersDummyStore = useUserDummyStore()
+  usersDummyStore.init()
 })
 
 const preview: Preview = {
