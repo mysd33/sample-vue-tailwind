849255a2ecc3d55ad1ca900faaba9192df711419
mysd33 <mysd.33@gmail.com>
2025-03-26 00:17:10 +0900

    storybook対応

--- src/usecases/login/views/MenuView.stories.ts
+++ src/usecases/login/views/MenuView.stories.ts
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 import MenuView from '@/usecases/login/views/MenuView.vue'
 import { AuthenticationServiceImpl } from '@/usecases/login/services/authenticationService'
 import { UserRepositoryImpl } from '@/usecases/common/repositories/userRepository'
+
 /**
  * ## メニュー画面
  */
